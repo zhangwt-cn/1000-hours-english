426204f6178fc250e128c0fcbdb86c8ccdeac9f6
zhangwentao <im.zhangwt@gmail.com>
2024-03-01 19:15:52 +0800

    Added Text-to-Speech functionality using OpenAI API and created English and Chinese practice materials

---
+++ TextToSpeech.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+
+# get local file cotent, for example: 2024/02-27
+
+
+path = "2024/03-01/"
+material_file = "english.md"
+material_path = path + material_file
+material_content = ""
+with open(material_path, "r") as file:
+    material_content = file.read()
+
+if material_content == "":
+    print("File not found or empty")
+    exit(1)
+
+
+client = OpenAI()
+response = client.audio.speech.create(
+    model="tts-1-hd",
+    voice="echo",
+    input=material_content,
+    response_format="flac"
+)
+
+response.write_to_file("{}material.mp3".format(path))
